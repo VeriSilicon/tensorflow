18ef129ca8936eaab7c643fdc25cb16706ea8de3
Zhao Xia <Zhao.Xia@verisilicon.com>
2017-06-02 13:01:04 +0800

    Fix input data signed issue.
    Convert singed char to unsigned char, before converting it to float.
    
    Signed-off-by: Zhao Xia <Zhao.Xia@verisilicon.com>

--- tensorflow/core/kernels/ovx/lenet_test.cc
+++ tensorflow/core/kernels/ovx/lenet_test.cc
@@ -148,7 +148,7 @@ namespace {
 
 #else
   GraphDef graph_def;
-  Status load_status = ReadBinaryProto(Env::Default(), "Lenet_OrigionGraph_weights.cook.pb", &graph_def);
+  Status load_status = ReadBinaryProto(Env::Default(), "Lenet_o.pb", &graph_def);
   ASSERT_TRUE(load_status.ok());
 //#else
 //  GraphDef graph_def;
@@ -202,18 +202,19 @@ namespace {
   //Tensor input_a(DT_FLOAT, {1, 28, 28, 1});
   //test::FillIota<float>(&input_a, 0.0f);
 
-  std::ifstream fin("7_1.raw", std::ios::binary);
+  std::ifstream fin("2_1.raw", std::ios::binary);
   char data[28*28] = {-1};
   fin.read(data, 28*28);
 
   Tensor input_a(DT_FLOAT, TensorShape({1, 28, 28, 1}));
-#if 0
   for(int i = 0; i < 28*28; i++) {
-    input_a.flat<float>().data()[i] = (float) data[i] / 256.f;
-    printf("%3d, ",data[i]);
+    input_a.flat<float>().data()[i] = (float) ((unsigned char)data[i]) / 256.f;
+#if 0
+    printf("%1.3f, ",input_a.flat<float>().data()[i]);
+    //printf("%3d, ",data[i]);
     if((i + 1 ) % 28 == 0)printf("\n");
-  }
 #endif
+  }
 
   std::vector<std::pair<string, Tensor>> inputs;
   inputs.emplace_back("input_image", input_a);
